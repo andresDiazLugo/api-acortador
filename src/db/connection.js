bf088302562219bec9296c84016ae7ee0c023ac8
genjutsu <andresd.ad5@gmail.com>
2022-10-09 17:57:35 -0300

    upload

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,11 +3,12 @@ require('dotenv').config()
 const link = require('../models/Link')
 
 
-const sequelize = new Sequelize('postgres://postgres:root@localhost:5432/acortador',
+const sequelize = new Sequelize('postgresql://postgres:2kNAf10GPQif3eIHCHaK@containers-us-west-74.railway.app:7664/railway',
 {
     logging:false
 }
 )
+
 
 link(sequelize)
 
