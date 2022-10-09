9244882514324129e39988798b0e9592987c8d40
genjutsu <andresd.ad5@gmail.com>
2022-10-09 17:03:33 -0300

    update connet postgres

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const link = require('../models/Link')
 
 
-const sequelize = new Sequelize('postgres://admin:zju17zX8idh2KTQyVkwlLgmuEL9RawsU@dpg-cd1h8qen6mprl48ir5h0-a/acortador',
+const sequelize = new Sequelize('postgres://admin:zju17zX8idh2KTQyVkwlLgmuEL9RawsU@dpg-cd1h8qen6mprl48ir5h0-a:5432/acortador',
 {
     logging:false
 }
